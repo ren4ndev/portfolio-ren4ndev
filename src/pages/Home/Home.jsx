1d538d2f44259a2d938b6b1e1f94cb2d007d4fdb
Renan Oliveira <oliveira.renan.rg@gmail.com>
2023-01-27 11:34:22 -0300

    feat: Add social media

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header';
 import InfoSection from '../../components/InfoSection';
 import AboutMeSection from '../../components/AboutMeSection/AboutMeSection';
 import SkillsSection from '../../components/SkillsSection/SkillsSection';
+import ContactModal from '../../components/ContactModal';
 
 function Home({
   theme,
@@ -12,6 +13,7 @@ function Home({
   return (
     <>
       <Header theme={theme} themeToggler={themeToggler} />
+      <ContactModal />
       <InfoSection />
       <AboutMeSection />
       <SkillsSection />
