6529571dd192248eb7ad14c84a5c0b414a196517
Renan Oliveira <oliveira.renan.rg@gmail.com>
2023-03-03 14:02:22 -0300

    refactor: Info Section content

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import GridSection from '../GridSection';
 import Typography from '../Typography/styles';
+import content from '../../locales/en.json';
 import {
   InfoContainer,
   MonoTextArea,
@@ -10,22 +11,34 @@ import {
 } from './styles';
 
 function InfoSection() {
+  const {
+    intro,
+    name,
+    title,
+    description,
+  } = content.infoSection;
   return (
     <GridSection color="primary">
       <InfoContainer>
         <MonoTextArea>
-          <Typography as="p" color="tertiary" type="body-02">Hi, my name is</Typography>
+          <Typography as="p" color="tertiary" type="body-02">
+            {intro}
+          </Typography>
         </MonoTextArea>
         <DisplayTextArea>
-          <Typography as="h2" color="textscale-04" type="display-01">Renan Oliveira.</Typography>
+          <Typography as="h2" color="textscale-04" type="display-01">
+            {name}
+          </Typography>
         </DisplayTextArea>
         <DisplayTextArea>
-          <Typography as="h2" color="textscale-06" type="display-01">I&apos;m a software developer</Typography>
+          <Typography as="h2" color="textscale-06" type="display-01">
+            {title}
+          </Typography>
         </DisplayTextArea>
         <DescriptionTextArea>
           <Typography as="p" color="textscale-05" type="body-02" weight="regular">
             <Typography color="quarternary" type="body-02" weight="regular">&lt;p&gt; </Typography>
-            I&apos;m a Full-stack developer, graduated at Trybe. My current goal is to keep improving my skills as a Python back-end developer, but there&apos;s a lot of other technologies that i know, work and that I&apos;m learning too. My permanent goal is to always be a Clean Coder.
+            {description}
             <Typography color="quarternary" type="body-02" weight="regular">&lt;/p&gt; </Typography>
           </Typography>
         </DescriptionTextArea>
