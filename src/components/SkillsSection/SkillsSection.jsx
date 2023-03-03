ff4a9315e46f1d01c4d0978f2535260d30266bed
Renan Oliveira <oliveira.renan.rg@gmail.com>
2023-03-03 14:28:51 -0300

    refactor: Skills Section content

--- src/components/SkillsSection/SkillsSection.jsx
+++ src/components/SkillsSection/SkillsSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GridSection from '../GridSection';
 import SkillsColumn from '../SkillsColumn';
+import content from '../../locales/en.json';
 import {
   SkillsContainer,
   GridContainer,
@@ -9,6 +10,7 @@ import {
 } from './styles';
 
 function SkillsSection() {
+  const { skillsSection } = content;
   return (
     <GridSection color="primary" id="skills">
       <GridContainer>
@@ -16,33 +18,17 @@ function SkillsSection() {
         <BottomColoredBox />
       </GridContainer>
       <SkillsContainer>
-        <SkillsColumn
-          icon="backend"
-          title="Backend Developer"
-          description="As a Backend developer, I manage and model databases, build API's and manage the flow of messages trough the cloud."
-          firstSubtitle="Languages i speak"
-          firstContent={['Python', 'SQL', 'Node.js']}
-          secondSubtitle="Devtools"
-          secondContent={['Express.js', 'MongoDB', 'MySQL', 'PostgreSQL', 'gRPC', 'Docker', 'Amazon Web Services', 'Git']}
-        />
-        <SkillsColumn
-          icon="frontend"
-          title="Frontend Developer"
-          description="As a Frontend developer, I build interfaces, manage the comunication back for front, and even design myself some of the interfaces i build."
-          firstSubtitle="Languages i speak"
-          firstContent={['Javascript', 'Python']}
-          secondSubtitle="Devtools"
-          secondContent={['React', 'React Native', 'Next.js', 'CSS-in-JS', 'Sass', 'HTML5', 'CSS3', 'Git']}
-        />
-        <SkillsColumn
-          icon="others"
-          title="Other skills"
-          description="As a software engineer, I&apos;ve worked with a lot of other technologies and patterns."
-          firstSubtitle="I&apos;ve already worked with:"
-          firstContent={['OOP (Object-Oriented Programming)', 'Micro services architecture', 'Airflow', 'Datadog', 'Gitflow', 'Atomic Design', 'Atomic Kitchen']}
-          secondSubtitle="I&apos;m familiar with:"
-          secondContent={['Clean architecture', 'Cloud']}
-        />
+        {skillsSection.map((item) => (
+          <SkillsColumn
+            icon={item.icon}
+            title={item.title}
+            description={item.description}
+            firstSubtitle={item.sublists[0].title}
+            firstContent={item.sublists[0].items}
+            secondSubtitle={item.sublists[1].title}
+            secondContent={item.sublists[1].items}
+          />
+        ))}
       </SkillsContainer>
     </GridSection>
   );
